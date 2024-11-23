29d34d1b936ee4d3f58452bc6d130014c05ad55b
AndreiCanedo <mec102092@gmail.com>
2024-11-23 12:50:00 -0700

    Servicio Cliente Creado

--- src/app/services/cliente.service.ts
+++ src/app/services/cliente.service.ts
@@ -1,9 +1,44 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { SharedService } from './shared.service';
+import { Observable, Subject } from 'rxjs';
+import { Cliente } from '../models/cliente.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClienteService {
 
-  constructor() { }
+  private sharedServices = inject(SharedService)
+
+  private endpoint = '/cliente';
+  private respField = 'Cliente';
+  private clienteCreadoSourse = new Subject<void>();
+
+  clienteCreado$ =  this.clienteCreadoSourse.asObservable();
+
+  notificarClienteCreado(){
+    this.clienteCreadoSourse.next();
+  }
+
+  cargarCliente():Observable<Cliente[]>{
+    return this.sharedServices.get<Cliente>(this.endpoint,this.respField);
+  }
+
+  cargarClienteById(id:string):Observable<Cliente>{
+    return this.sharedServices.getById<Cliente>(`${this.endpoint}/${id}`, this.respField)
+  }
+
+  crearCliente(cliente:Cliente):Observable<Cliente>{
+    return this.sharedServices.post<Cliente>(this.endpoint,cliente)
+  }
+
+  actualizarCliente(cliente:Cliente, id:String):Observable<Cliente>{
+    return this.sharedServices.put<Cliente>(`${this.endpoint}/${id}`,cliente)
+  }
+
+  eliminarCliente(id:string):Observable<Cliente>{
+    return this.sharedServices.delete<Cliente>(`${this.endpoint}/${id}` )
+  }
+
+
 }
